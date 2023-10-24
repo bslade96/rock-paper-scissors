390991c49e79544973837fe3ea32349b6f3292f1
Brandi Slade <146174174+bslade96@users.noreply.github.com>
2023-10-23 18:41:43 -0600

    start afresh

--- javascript.js
+++
@@ -1,8 +0,0 @@
-const gameOptions = ['rock', 'paper', 'scissors']    
-    
-function getComputerChoice() {
-    return gameOptions[Math.floor(Math.random() * gameOptions.length)];
-}
-
-console.log(getComputerChoice('rock'))
-
