45bc67b14c29e2a072b7980a5a137f7ed868c2f7
Brandi Slade <146174174+bslade96@users.noreply.github.com>
2023-10-26 04:57:11 -0600

    Update to add score function

--- javascript.js
+++ javascript.js
@@ -1,31 +1,60 @@
-'use strict'
-
+//computer choice 
 function getComputerChoice() {
-    let randomChoice = (arr[Math.floor(Math.random() * arr.length)])
-    return randomChoice
+    let array = ['rock', 'paper', 'scissors']
+    return array[Math.floor(Math.random() * array.length)].toString()
 }
 
+//Round
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
+
     if (playerSelection === computerSelection) {
-        return 'It\'s a tie!'
-    } else if ((playerSelection === 'paper') && (computerSelection === 'scissors')) {
-        return 'You Lose! Scissors beats Paper'
+        return 'It\'s a tie. Play the next round.'
     } else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
-        return 'You Lose! Paper beats Rock'
+        return 'You lose this round. Paper beats rock.'
+    } else if ((playerSelection === 'paper') && (computerSelection === 'scissors')) {
+        return 'You lose this round. Scissors beats paper.'
     } else if ((playerSelection === 'scissors') && (computerSelection === 'rock')) {
-        return 'You Lose! Rock beats Scissors'
-    } else if (!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors')) {
-        return 'Error: Please enter either "rock", "paper", or "scissors"'
+        return 'You lose this round. Rock beats scissors.'
+        //add a prompt? how do I get it to loop back to if else again?
+    } if (!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors')) {
+        return 'Invalid entry: Please type "rock", "paper", or "scissors"'
     } else {
-        return 'YOU WIN!!!'
+        return ('You win this round!')
     }
 }
 
-const arr = ['rock', 'paper', 'scissors']
-const playerSelection = 'papeR'
-const computerSelection = getComputerChoice()
+//game of 5 rounds that 
+//DONE: keeps score and 
+//DONE: reports a winner or loser at the end
+function game() {
+    let computerScore = 0
+    let playerScore = 0
+    
+    let winnerOfRound = playRound(playerSelection, computerSelection) 
+    if (winnerOfRound.includes('win')) {
+        playerScore++
+        return `Your score: ${playerScore}    Computer score: ${computerScore}`
+    } else if (winnerOfRound.includes('lose')) {
+        computerScore++
+        return `Your score: ${playerScore}    Computer score: ${computerScore}`
+    } else if (winnerOfRound.includes('tie')) {
+        return `Your score: ${playerScore}    Computer score: ${computerScore}`
+    } 
+}
+
+computerSelection = getComputerChoice()
+playerSelection = prompt('Rock, Paper, Scissors: SHOOT!', 'RO')
+    playerSelection = playerSelection.toLowerCase()
 
 console.log(computerSelection)
 console.log(playerSelection)
 console.log(playRound(playerSelection, computerSelection))
+console.log(game())
+
+//if (playerScore === computerScore) {
+    //return 'It\'s a draw. No one wins the game.'
+//} else if (playerScore > computerScore) {
+    //return 'YOU WIN THE GAME!'
+//} else {
+    //return 'You lost the game.'
+//}
